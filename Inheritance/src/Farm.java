7f907550fad9dc71033265dadda6507a1ba978e3
LefL9 <76185105+LefL9@users.noreply.github.com>
2025-01-31 14:42:21 -0700

    1/31/2025

--- Inheritance/src/Farm.java
+++ Inheritance/src/Farm.java
@@ -9,7 +9,20 @@ public class Farm {
 		Animal[] animals = new Animal[3];
 		animals[0] = johnston;
 		
+		String animalType = identifyAnimal(animals[1]);
+		System.out.println("This animal is a" + animalType);
 		
+		public static String identifyAnimal(Animal type) {
+			if (anim instanceof Horse) {
+				return "Horse";
+			}
+			if (anim instanceof Cat) {
+				return "Cat";
+			}
+			return null;
+		}
 	}
+
+
 
 }
