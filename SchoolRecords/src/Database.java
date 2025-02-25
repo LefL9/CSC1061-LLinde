cd9527f57970a02123e4b9eaaa8bad078913be12
LefL9 <76185105+LefL9@users.noreply.github.com>
2025-02-24 17:37:31 -0700

    2/24/2025

--- SchoolRecords/src/Database.java
+++ SchoolRecords/src/Database.java
@@ -1,6 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Database {
-	public void writePerson(Person person){
+	
+	public void writePerson(Person person) {
+		File personFile = new File("PersonDatabase.txt");
+		if (!personFile.exists()) {
+			try {
+				personFile.createNewFile();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Cannot write to " + personFile.getName());
+				System.exit(-1);
+			}
+
+		}
+		PrintWriter pWriter = null;
+		try {
+			pWriter = new PrintWriter(personFile.getName());
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.exit(-1);
+		}
+		
+		
 		
 	}
 }
